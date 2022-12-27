da403e547efbc86e65bd278a0810f948aa172859
NitishH27 <100903164+NitishH27@users.noreply.github.com>
2022-12-27 08:06:09 +0000

    done currency

--- currency.js
+++ currency.js
@@ -5,25 +5,25 @@ var ip
 
 do{
 
-    console.log("1. Press 1 to Convert Currency from Dollar TO Rupee");
+    console.log("1. Press 1 to Convert Currency from Dollar TO Rupee ");
 
-    console.log("2. Press 2 to Convert Currency from Rupee TO Dollar");
+    console.log("2. Press 2 to Convert Currency from Rupee TO Dollar ");
 
-    var ch = parseInt(r1.question("Enter your choice:"));
+    var ch = parseInt(r1.question("Enter your choice: "));
 
     switch(ch){
 
         case 1:
              ip = r1.question("Enter amount in Dollars");
-            console.log("Amount in rupees: " ,CC.dollarToRupees(ip));
+            console.log("Amount in rupees : " ,CC.dollarToRupees(ip));
 
           
             break;
 
         case 2:
 
-              ip = r1.question("Enter amount in Rupees");
-             console.log("Amount in rupees: " ,CC.rupeesToDollar(ip));
+              ip = r1.question("Enter amount in Rupees ");
+             console.log("Amount in rupees : " ,CC.rupeesToDollar(ip));
 
             break;
 
